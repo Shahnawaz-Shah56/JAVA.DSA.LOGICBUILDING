7f77c7fe7abae0ff5fd9d541465702c23ea174c7
Shahnawaz Shah <shahcreation6158@gmail.com>
2026-09-25 00:51:46 +0530

    pushing first time in intelli j and first files of the dsa journey

---
+++ src/FOR_LOOP/program0.java
@@ -0,0 +1,17 @@
+package FOR_LOOP;
+
+
+
+//Question 1 "1 TO N PRINTING "
+
+
+public class program0 {
+    public static void main(String[] args){
+
+        int n = 10;
+
+        for (int i = 0; i <= n;i++){
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/FOR_LOOP/program1.java
@@ -0,0 +1,15 @@
+package FOR_LOOP;
+//Question 2 "N TO 1 PRINTING "
+
+
+
+public class program1 {
+    public static void main(String[] args) {
+
+
+                for (int n = 10; n >= 0;n--){
+                    System.out.println(n);
+                }
+            }
+        }
+
