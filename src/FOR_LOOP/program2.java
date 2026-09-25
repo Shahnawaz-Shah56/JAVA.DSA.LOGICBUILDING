3d149d4ce6903c1c9327e11a850fc7c6eac3e1ef
Shahnawaz Shah <shahcreation6158@gmail.com>
2026-09-25 22:04:17 +0530

    Another question of for loop for dsa logic building

---
+++ src/FOR_LOOP/program2.java
@@ -0,0 +1,22 @@
+package FOR_LOOP;
+
+public class program2 {
+    public static void main(String[] args) {
+
+    int sum = 0;
+    int n = 5;
+        for (int i = 1; i <= n; i++) {
+
+            sum = sum + i;
+
+
+
+//
+        }
+        System.out.println(sum);
+    }
+
+}
+
+/* IN THIS CODE I FIXED A MISTAKE IN MY CODE I WASN'T GETTING THE RESULT I WANTED,
+WAS GETTING BUT IN OTHER WAY . SO IT WAS 5 TO 10 MINS STRUGGLE FOR CATCHING THAT CLUE */
--- src/FOR_LOOP/program3.java
+++ src/FOR_LOOP/program3.java
@@ -2,21 +2,13 @@ package FOR_LOOP;
 
 public class program3 {
     public static void main(String[] args) {
+        int n = 18;
+        for (int i = 1; i < n; i++) {
+            if(n % i  == 0){
+                System.out.println(i + " is the factor of the " + n);
+            }
 
-    int sum = 0;
-    int n = 5;
-        for (int i = 1; i <= n; i++) {
-
-            sum = sum + i;
-
-
-
-//
         }
-        System.out.println(sum);
     }
 
 }
-
-/* IN THIS CODE I FIXED A MISTAKE IN MY CODE I WASN'T GETTING THE RESULT I WANTED,
-WAS GETTING BUT IN OTHER WAY . SO IT WAS 5 TO 10 MINS STRUGGLE FOR CATCHING THAT CLUE */
